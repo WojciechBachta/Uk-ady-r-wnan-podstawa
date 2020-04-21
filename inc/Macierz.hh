9de88c32f749fcfcbb4452a73ef7e531768abfe6
WojciechBachta <62569872+WojciechBachta@users.noreply.github.com>
2020-04-21 16:55:35 +0200

    Modyfikacja Macierz 10x10

--- inc/Macierz.hh
+++ inc/Macierz.hh
@@ -4,19 +4,52 @@
 #include "rozmiar.h"
 #include <iostream>
 
+#include "Wektor.hh"
+using namespace std;
 
 /*
  *  Tutaj trzeba opisac klase. Jakie pojecie modeluje ta klasa
  *  i jakie ma glowne cechy.
  */
 class Macierz {
+
+    Wektor _SkladnikiM[ROZMIAR];
+
   /*
    *  Tutaj trzeba wstawic definicje odpowiednich pol i metod prywatnych
    */
   public:
+
+
+   Wektor WezWiersz(int wiersz)const{
+   return _SkladnikiM[wiersz];
+   }
+   void ZamienWiersz(int wiersz, Wektor W){
+    _SkladnikiM[wiersz]=W;
+   }
+    const Wektor & operator[] (int index) const{
+    return _SkladnikiM[index];
+    }
+    Wektor & operator[] (int index){
+    return _SkladnikiM[index];
+    }
+
+    Macierz(); //jednostkowa
+
+    Macierz operator + (const Macierz & M) const;
+    Macierz operator - (const Macierz & M) const;
+    Macierz operator * (const double l) const;
+    Macierz operator * (const Macierz & M) const;
+    Wektor operator * (const Wektor W) const;
+
+    const Macierz MacierzOdwrotna() const;
+    const Macierz operator * () const;
+    const double Wyznacznik() const;
+
   /*
    *  Tutaj trzeba wstawic definicje odpowiednich metod publicznych
-   */    
+   */
+
 };
 
 
@@ -25,7 +58,7 @@ class Macierz {
  * danych akceptuje. Jakie jest znaczenie parametrow itd.
  * Szczegoly dotyczace zalecen realizacji opisow mozna
  * znalezc w pliku:
- *    ~bk/edu/kpo/zalecenia.txt 
+ *    ~bk/edu/kpo/zalecenia.txt
  */
 std::istream& operator >> (std::istream &Strm, Macierz &Mac);
 
@@ -34,7 +67,7 @@ std::istream& operator >> (std::istream &Strm, Macierz &Mac);
  * danych akceptuje. Jakie jest znaczenie parametrow itd.
  * Szczegoly dotyczace zalecen realizacji opisow mozna
  * znalezc w pliku:
- *    ~bk/edu/kpo/zalecenia.txt 
+ *    ~bk/edu/kpo/zalecenia.txt
  */
 std::ostream& operator << (std::ostream &Strm, const Macierz &Mac);
 
